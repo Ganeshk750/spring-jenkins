1a06c808f97d759ce514be30d4484dddd16ad8ea
Ganeshk750 <ganesh9ot@gmail.com>
2020-07-30 10:09:28 +0530

    Notification On

--- src/test/java/com/ganesh/SpringJenkinsApplicationTests.java
+++ src/test/java/com/ganesh/SpringJenkinsApplicationTests.java
@@ -14,7 +14,7 @@ class SpringJenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
-		logger.info("<=====Test Method executed =====>");
+		logger.info("<=====Test Method executed=====>");
 		assertEquals(true, true);
 	}
 
